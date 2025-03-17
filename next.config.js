44114d99ba8e5612a5e1ad369975a74d9412d9ba
Te1epuzik <tonkiyslonik@gmail.com>
2025-03-17 21:16:40 +0300

    chore(next.config.js): configuration for github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	output: "export",
+	basePath: "/next-lms",
+	reactStringMode: true,
+}
 
 module.exports = nextConfig
