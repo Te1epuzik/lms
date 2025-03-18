e9b2f5a2704d7869b5af75e639c1a5efa8b53504
Te1epuzik <tonkiyslonik@gmail.com>
2025-03-19 00:07:55 +0300

    feat(authStore): persist for authStore

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,20 +1,26 @@
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
 import type { TAuthStore } from "./types";
 
 export const useAuthStore = create<TAuthStore>()(
-  devtools((set) => ({
-    token: null,
-    isAuth: false,
-    name: null,
-    role: null,
-    signIn: (token: string | null, role: "student" | "teacher") =>
-      set({ token, isAuth: !!token, role }, undefined, "auth/signIn"),
-    signOut: () =>
-      set(
-        { token: null, isAuth: false, role: null },
-        undefined,
-        "auth/signOut",
-      ),
-  })),
+  persist(
+    devtools((set) => ({
+      token: null,
+      isAuth: false,
+      name: null,
+      role: null,
+      signIn: (token: string | null, role: "student" | "teacher") =>
+        set({ token, isAuth: !!token, role }, undefined, "auth/signIn"),
+      signOut: () =>
+        set(
+          { token: null, isAuth: false, role: null },
+          undefined,
+          "auth/signOut",
+        ),
+    })),
+    {
+      name: "auth-storage",
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
 );
