9be8ddfc7a07971fbb050fc03b321504aac6279d
Te1epuzik <tonkiyslonik@gmail.com>
2025-03-18 18:34:37 +0300

    refactor: padding variables & CustomLink fix
    	CustomLink is client component now
    	Added scss variables:
    $padding-small: 0.8rem (8px on root font size is 10px);
    $padding-medium: 1rem;
    $padding-big: 2rem;

--- src/shared/CustomLink/components/CustomLink.tsx
+++ src/shared/CustomLink/components/CustomLink.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { ComponentPropsWithoutRef } from "react";
 import Link, { LinkProps } from "next/link";
 import { handlePointerDown, handleDismiss } from "@/utils/activeEmulation";
